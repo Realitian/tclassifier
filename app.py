e3f0b95403d93be7de0f4ae467d970b19698c2a5
Realitian <infinite.reality.solution@gmail.com>
2018-12-14 13:23:53 -0800

    add daemon

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template, request
 # from flask_heroku import Heroku
 from engine.main import Service
 import json
-from daemon import Daemon
 
 app = Flask(__name__)
 # heroku = Heroku(app)
 service = Service('./engine')
-# daemon = Daemon()
 
 @app.route('/')
 def index():
@@ -24,6 +22,5 @@ def query():
 
 if __name__ == '__main__':
     #app.debug = True
-    # daemon.run()
     service.query("Microsoft Co. $MSFT Shares Sold by American Research &amp")
     app.run()
