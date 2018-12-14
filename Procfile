c12ba1dc15a7fe23c1e783241594e94fb4bd9532
Realitian <infinite.reality.solution@gmail.com>
2018-12-14 11:29:42 -0800

    change profile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:daemon
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -3,24 +3,27 @@ from flask import Flask, render_template, request
 # from flask_heroku import Heroku
 from engine.main import Service
 import json
+from daemon import Daemon
 
 app = Flask(__name__)
 # heroku = Heroku(app)
-service = Service('./engine')
+# service = Service('./engine')
+daemon = Daemon()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/query', methods=['POST'])
-def query():
-    text = None
-    if request.method == 'POST':
-        text = request.form['text']
-
-    return service.query(str(text))
+# @app.route('/query', methods=['POST'])
+# def query():
+#     text = None
+#     if request.method == 'POST':
+#         text = request.form['text']
+#
+#     return service.query(str(text))
 
 if __name__ == '__main__':
     #app.debug = True
-    service.query("Microsoft Co. $MSFT Shares Sold by American Research &amp")
+    daemon.run()
+    # service.query("Microsoft Co. $MSFT Shares Sold by American Research &amp")
     app.run()
